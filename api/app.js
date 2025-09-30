e372392e77c89681652361b4a2df412e23eadfa5
trvv <trhuvan@gmail.com>
2025-09-30 17:57:10 +0700

    connect resgister api

--- api/app.js
+++ api/app.js
@@ -1,15 +1,25 @@
-import express from "express";
-import authRouter from "./routes/auth.route.js";
+import express from 'express';
+import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
-console.log("===========Starting Server===========");
+console.log('===========Starting Server===========');
 
 // Middleware để parse JSON body
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL || 'http://localhost:5173',
+        credentials: true,
+    })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use("/api/auth", authRouter);
+app.use(cookieParser());
+app.use('/api/auth', authRouter);
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+    console.log('Server is running on port 3000');
 });
--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,12 +1,36 @@
 import bcrypt from 'bcrypt';
 import prisma from '../lib/prisma.js';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const register = async (req, res) => {
     const { username, email, password } = req.body;
 
     try {
+        // Kiểm tra xem username đã tồn tại hay chưa
+        const existingUsername = await prisma.user.findUnique({
+            where: {
+                username,
+            },
+        });
+        if (existingUsername) {
+            return res.status(400).json({ message: 'Username already exists' });
+        }
+
+        // Kiểm tra xem email đã tồn tại hay chưa
+        const existingEmail = await prisma.user.findUnique({
+            where: {
+                email,
+            },
+        });
+        if (existingEmail) {
+            return res.status(400).json({ message: 'Email already exists' });
+        }
+
         //   Hash password
         const hashedPassword = await bcrypt.hash(password, 10);
+        console.log('Hashed password:', hashedPassword);
 
         //   Tạo một người dùng mới rồi lưu vào database
         const newUser = await prisma.user.create({
@@ -17,20 +41,77 @@ export const register = async (req, res) => {
                 avatar: '',
             },
         });
-    } catch (error) {}
 
-    res.status(201).json({
-        message: 'User created successfully',
-        username,
-        email,
-        hashedPassword,
-    });
+        res.status(201).json({
+            message: 'User created successfully',
+        });
+    } catch (error) {
+        console.error('Register user error:', error);
+
+        res.status(500).json({
+            message: 'Failed to register user',
+        });
+    }
 };
 
-export const login = (req, res) => {
-    res.json({ message: 'Hello World from auth controller' });
+export const login = async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        // Kiểm tra xem user có tồn tại hay không
+        const user = await prisma.user.findUnique({
+            where: {
+                username,
+            },
+        });
+
+        if (!user)
+            return res
+                .status(401)
+                .json({ message: 'Invalid username or password' });
+
+        // Kiểm tra xem password có đúng hay không
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordCorrect)
+            return res
+                .status(401)
+                .json({ message: 'Invalid username or password' });
+
+        // Tạo cookie token và gửi đến client
+        const age = 1000 * 60 * 60 * 24 * 7; // 7 days
+
+        const token = jwt.sign(
+            {
+                id: user.id,
+            },
+            process.env.JWT_SECRET_KEY,
+            { expiresIn: age }
+        );
+
+        res.cookie('token', token, {
+            httpOnly: true,
+            // secure: true,
+            maxAge: age,
+        })
+            .status(200)
+            .json({ message: 'Login successful' });
+    } catch (error) {
+        console.error('Login user error:', error);
+
+        res.status(500).json({
+            message: 'Failed to login user',
+        });
+    }
 };
 
 export const logout = (req, res) => {
-    res.json({ message: 'Hello World from auth controller' });
+    try {
+        res.clearCookie('token')
+            .status(200)
+            .json({ message: 'Logout successful' });
+    } catch (error) {
+        console.error('Logout user error:', error);
+        res.status(500).json({ message: 'Failed to logout user' });
+    }
 };
